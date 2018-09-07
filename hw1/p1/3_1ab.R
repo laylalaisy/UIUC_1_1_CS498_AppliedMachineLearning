689049766f4e82909c443027ec679d3dc25ac083
laylalaisy <lsylayla@163.com>
2018-09-07 11:02:34 -0700

    finish p1A

--- hw1/p1/3_1ab.R
+++ hw1/p1/3_1ab.R
@@ -28,7 +28,7 @@ for (iter in 1:iters)
   # extract train data by randomly assigning 80% of the data to train
   train_index <- createDataPartition(y=input_y, p=.8, list=FALSE)
 
-  # extract feature vectors and labels
+  # extract training feature vectors and labels
   train_x <- input_x[train_index, ]
   train_y <- input_y[train_index]
 
@@ -52,40 +52,35 @@ for (iter in 1:iters)
   train_x_positive_scaled <- t(t(train_x_positive_offset) / train_x_positive_sd)
   train_x_positive_log_prob <- -(1/2)*apply(train_x_positive_scaled, 1, function(x){sum(x^2)}) - sum(log(train_x_positive_sd))
 
-  print(dim(train_x_positive_log_prob))
   train_x_negative_offset <- t(t(train_x) - train_x_negative_mean)
   train_x_negative_scaled <- t(t(train_x_negative_offset) / train_x_negative_sd)
   train_x_negative_log_prob <- -(1/2)*apply(train_x_negative_scaled, 1, function(x){sum(x^2)}) - sum(log(train_x_negative_sd))
 
    # record percentage guessed correctly in scores array
-   predict_y <- train_x_positive_log_prob > train_x_negative_log_prob
-   print(predict_y)
-   train_correct_account <- predict_y == train_y
-   train_accuracy[iter]<-sum(train_correct_account)/(sum(train_correct_account)+sum(!train_correct_account))
+   train_predict_y <- train_x_positive_log_prob > train_x_negative_log_prob
+   train_correct_account <- train_predict_y == train_y
+   train_accuracy[iter] <- sum(train_correct_account)/(sum(train_correct_account)+sum(!train_correct_account))
 
-   print(train_accuracy[iter])
+   # TEST
+   # extract feature vectors and labels
+   test_x <- input_x[-train_index, ]
+   test_y <- input_y[-train_index]
 
-   # ## evaluating - testing data
-   #
-   # # extract *testing* feature vectors and labels
-   # x_vec_test<-x_vec[-wtd, ]
-   # y_vec_test<-y_vec[-wtd]
-   #
-   # # Solve for log probability that each feature vector corresponds to a positive label
-   # pos_test_offset<-t(t(x_vec_test)-pos_mean)
-   # pos_test_scaled<-t(t(pos_test_offset)/pos_sd)
-   # pos_test_log_prob<--(1/2)*rowSums(apply(pos_test_scaled, c(1, 2), function(x)x^2), na.rm=TRUE)-sum(log(pos_sd))
-   #
-   # # Solve for log probability that each feature vector corresponds to a negative label
-   # neg_test_offset<-t(t(x_vec_test)-neg_mean)
-   # neg_test_scaled<-t(t(neg_test_offset)/neg_sd)
-   # neg_test_log_prob<--(1/2)*rowSums(apply(neg_test_scaled, c(1, 2), function(x)x^2), na.rm=TRUE)-sum(log(neg_sd))
-   #
-   # # record percentage guessed correctly in scores array
-   # guesses_test<-pos_test_log_prob>neg_test_log_prob
-   # num_correct_test<-guesses_test==y_vec_test
-   # test_score[iter]<-sum(num_correct_test)/(sum(num_correct_test)+sum(!num_correct_test))
+   # calculate log probability
+   test_x_positive_offset <- t(t(test_x) - train_x_positive_mean)
+   test_x_positive_scaled <- t(t(test_x_positive_offset) / train_x_positive_sd)
+   test_x_positive_log_prob <- -(1/2)*apply(test_x_positive_scaled, 1, function(x){sum(x^2)}) - sum(log(train_x_positive_sd))
+
+   test_x_negative_offset <- t(t(test_x) - train_x_negative_mean)
+   test_x_negative_scaled <- t(t(test_x_negative_offset) / train_x_negative_sd)
+   test_x_negative_log_prob <- -(1/2)*apply(test_x_negative_scaled, 1, function(x){sum(x^2)}) - sum(log(train_x_negative_sd))
+
+   # record percentage guessed correctly in scores array
+   test_predict_y <- test_x_positive_log_prob > test_x_negative_log_prob
+   test_correct_account <- test_predict_y == test_y
+   test_accuracy[iter] <- sum(test_correct_account)/(sum(test_correct_account)+sum(!test_correct_account))
  }
 
-# print(test_score)
+result <- mean(test_accuracy)
+print(result)
 
