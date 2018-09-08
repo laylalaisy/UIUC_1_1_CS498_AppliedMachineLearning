19d7cf880c921a1e2255378cd29d4b463828ddc6
laylalaisy <lsylayla@163.com>
2018-09-08 14:55:10 -0700

    doing p2

---
+++ hw1/p2/2A.py
@@ -0,0 +1,20 @@
+import csv
+import numpy as np
+
+if __name__ == "__main__":
+
+    # read in input_data
+    with open("./train.csv", "r") as input_file:
+        input_reader = csv.reader(input_file)
+
+        input_data_list = []
+        for row in input_reader:
+            input_data_list.append(row)
+
+        input_file.close()
+
+    input_data = np.array(input_data_list)
+    input_x = input_data[2:][:]
+    input_y = input_data[1][1:]
+
+    print(input_data[1][1])
