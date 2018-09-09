ca090982b19569e64c6ee435c31fba2a87bd19e4
laylalaisy <lsylayla@163.com>
2018-09-08 17:17:57 -0700

    read in test data

--- hw1/p2/2A.py
+++ hw1/p2/2A.py
@@ -1,24 +1,42 @@
 import csv
 import numpy as np
 
+
+
 if __name__ == "__main__":
 
-    # read in training csv file
-    with open("./train.csv", "r") as input_file:
-        input_reader = csv.reader(input_file)
-        # read in training data
-        input_data_list = []
-        for row in input_reader:
-            input_data_list.append(row)
-        # close training csv file
-        input_file.close()
+    # read in train csv file
+    with open("./train.csv", "r") as train_input_file:
+        train_input_reader = csv.reader(train_input_file)
+        # read in train data
+        train_input_data_list = []
+        for row in train_input_reader:
+            train_input_data_list.append(row)
+        # close train csv file
+        train_input_file.close()
 
     # change input data from list to array
-    input_data = np.array(input_data_list)
+    train_input_data = np.array(train_input_data_list)
 
     # extract data of feature and label
-    input_x = input_data[1:, 2:]
-    input_y = input_data[1:, 1:2]
+    train_input_x = train_input_data[1:, 2:]
+    train_input_y = train_input_data[1:, 1:2]
 
-    print(input_x.shape)
-    print(input_y.shape)
+    # read in test csv file
+    with open("./test.csv", "r") as test_input_file:
+        test_input_reader = csv.reader(test_input_file)
+        # read in test data
+        test_input_data_list = []
+        for row in test_input_reader:
+            test_input_data_list.append(row)
+        # close test csv file
+        test_input_file.close()
+
+    # change input data from list to array
+    test_input_x = np.array(test_input_data_list)
+
+
+
+
+
+
