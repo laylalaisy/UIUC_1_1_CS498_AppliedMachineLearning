c61e5300daa2623d43ea7969a1e7510d182c71ee
laylalaisy <lsylayla@163.com>
2018-09-08 22:51:08 -0700

    done data

--- hw1/p2/2A.py
+++ hw1/p2/2A.py
@@ -1,7 +1,19 @@
 import csv
 import numpy as np
 
-# def gaussianNaiveBayes(train_input_x, train_input_y, test_input_x):
+class_amount = 10
+
+def gaussianNaiveBayes(train_input_x_classified, test_input_x):
+    # initialize
+    initial_value = 0
+    list_length = 10
+    train_mean = [initial_value] * list_length
+    train_sd = [initial_value] * list_length
+
+    for iter in range(class_amount):
+        #train_mean[iter] = np.mean(train_input_x_classified[iter])
+        print(train_input_x_classified[iter][0][0])
+        break
 
 
 if __name__ == "__main__":
@@ -25,19 +37,16 @@ if __name__ == "__main__":
 
 
     # STORE CLASSIFIED TRAINING DATA
-    test_input_x = np.array(test_input_data_list)
-
-    train_input_x_classified = []
-    for iter in range(10):
-        train_input_x_classified.append([])
+    train_input_x_classified_list = []
+    for iter in range(class_amount):
+        train_input_x_classified_list.append([])
 
     train_input_sample_amount = train_input_y.shape[0]
 
     for iter in range(train_input_sample_amount):
-        train_input_x_classified[int(train_input_y[iter])].append(train_input_x[iter])
+        train_input_x_classified_list[int(train_input_y[iter])].append(train_input_x[iter])
 
-    print(train_input_x_classified)
-
+    train_input_x_classified = np.array(train_input_x_classified_list)
 
     # READ IN TESTING DATA
     with open("./test.csv", "r") as test_input_file:
@@ -48,6 +57,11 @@ if __name__ == "__main__":
             test_input_data_list.append(row)
         # close testing csv file
         test_input_file.close()
+
+    test_input_x = np.array(test_input_data_list)
+
+    # GAUSSIAN
+    gaussianNaiveBayes(train_input_x_classified, test_input_x)
 
 
 
