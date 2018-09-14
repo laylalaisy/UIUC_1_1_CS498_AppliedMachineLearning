c03b3d02e7cd8cc10bfe57e0ea8055175eb936f6
laylalaisy <lsylayla@163.com>
2018-09-14 12:26:28 -0700

    update

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -2,6 +2,17 @@ import csv
 from sklearn import preprocessing
 import numpy as np
 
+def stochasticGradientDescent(trainX, trainY, validationX, validationY, regularizer, step_length):
+    [train_sample_amount, train_feature_amount] = trainX.shape
+
+
+    ## INITIALIZE a AND b
+    a = np.ones([train_feature_amount]).astype(float)
+    b = 1
+
+    #for step in range(train_input_x):
+    print(a)
+
 if __name__ == "__main__":
 
     ## READ IN TRAINING DATA
@@ -43,13 +54,24 @@ if __name__ == "__main__":
     train_input_x_rescaled = preprocessing.StandardScaler(train_input_x[:, 1:train_feature_amount-2], with_mean=True, with_std=True)
     test_input_x_rescaled = preprocessing.StandardScaler(test_input_x[:, 1:], with_mean=True, with_std=True)
 
-    amount_epoch = 5
+    ## TRAIN
+    amount_epoch = 1
     amount_step = 300
     amount_validation = 50
-    for i in range(amount_epoch):
+    for iter_epoch in range(amount_epoch):
+        # data of the whole epoch
         index_epoch = np.random.choice(train_sample_amount, size=amount_step + amount_validation, replace=False)
         train_input_x_epoch = train_input_x[index_epoch, :]
+        train_input_y_epoch = train_input_y[index_epoch]
+
+        # training data and validation data
         index_step = np.random.choice(train_input_x_epoch.shape[0], size=amount_step, replace=False)
         train_input_x_step = train_input_x_epoch[index_step, :]
-        index_validation = np.delete(train_input_x_epoch, index_step, axis=0)
+        train_input_y_step = train_input_y_epoch[index_step]
+        train_input_x__validation = np.delete(train_input_x_epoch, index_step, axis=0)
+        train_input_y__validation = np.delete(train_input_y_epoch, index_step, axis=0)
+
+        # step_length
+        step_length = 1.0 / ((0.01 * iter_epoch) + 50)
+        stochasticGradientDescent(train_input_x_step, train_input_y_step, train_input_x__validation, train_input_y__validation, 1, step_length)
 
