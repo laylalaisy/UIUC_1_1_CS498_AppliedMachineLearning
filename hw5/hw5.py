a7d994c74f4ae570aceb627d2fd340e562b67f93
laylalaisy <lsylayla@163.com>
2018-10-10 07:45:05 -0700

    update hw5

--- hw5/hw5.py
+++ hw5/hw5.py
@@ -1,27 +1,40 @@
 import os
+import numpy as np
+import scipy as sp
+from sklearn import cluster
+import matplotlib.pyplot as plt
 
-
-if __name__ == "__main__":
-    path1 = './HMP_Dataset'
+def readData(path1):
     folders = os.listdir(path1)
 
     activities = []
     for i in range(len(folders)):
-        path2 = ""+path1+"/"+folders[i]
+        activity = []
+        path2 = "" + path1 + "/" + folders[i]
         files = os.listdir(path2)
         for file in files:
             if not os.path.isdir(file):
-                with open(""+path2+"/"+file, 'r') as f:
+                with open("" + path2 + "/" + file, 'r') as f:
                     for line in f.readlines():
-                        activity = []
+                        current = []
                         num = str(line).rstrip("\r\n").split(" ")
-                        activity.append(int(num[0]))
-                        activity.append(int(num[1]))
-                        activity.append(int(num[2]))
-                        activity.append(i)
-                        activities.append(activity)
+                        current.append(int(num[0]))
+                        current.append(int(num[1]))
+                        current.append(int(num[2]))
+                        current.append(i)
+                        current = np.array(current)
+                        activity.append(current)
+        activity = np.array(activity)
+        activities.append(activity)
+    activities = np.array(activities)
 
-    print(activities)
+    print(activities[0])
+
+
+
+if __name__ == "__main__":
+    readData('./HMP_Dataset')
+
 
 
 
