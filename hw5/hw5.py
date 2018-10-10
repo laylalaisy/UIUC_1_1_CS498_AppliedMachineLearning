2d370c834c914d216f43b8a1790bec5cfe29f2ba
laylalaisy <lsylayla@163.com>
2018-10-10 10:13:01 -0700

    flatten

--- hw5/hw5.py
+++ hw5/hw5.py
@@ -1,13 +1,17 @@
 import os
+import math
 import numpy as np
 import scipy as sp
 from sklearn import cluster
-from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
 act_num = 14
 
 def readData(path1):
+    # activities: data of all activities
+    # activity: data of each activity
+    # cur_file: data of each file
+    # cur_act: data of each line
     folders = os.listdir(path1)
 
     activities = []
@@ -16,27 +20,48 @@ def readData(path1):
         path2 = "" + path1 + "/" + folders[i]
         files = os.listdir(path2)
         for file in files:
+            cur_file = []
             if not os.path.isdir(file):
                 with open("" + path2 + "/" + file, 'r') as f:
                     for line in f.readlines():
-                        current = []
+                        cur_act = []
                         num = str(line).rstrip("\r\n").split(" ")
-                        current.append(int(num[0]))
-                        current.append(int(num[1]))
-                        current.append(int(num[2]))
-                        current.append(i)
-                        current = np.array(current)
-                        activity.append(current)
+                        cur_act.append(int(num[0]))
+                        cur_act.append(int(num[1]))
+                        cur_act.append(int(num[2]))
+                        cur_act.append(i)
+                        cur_act = np.array(cur_act)
+                        cur_file.append(cur_act)
+                    cur_file = np.array(cur_file)
+                    activity.append(cur_file)
         activity = np.array(activity)
         activities.append(activity)
-    activities = np.array(activities)
 
+    activities = np.array(activities)
     return folders, activities
 
 def split(act_data, percent, segment_size):
-    for i in range(act_num):
-        length = act_data[i].shape[0]
-        print(length)
+    activities = []
+    train_activities = []
+    test_activities = []
+
+    for i in range(act_num): # each activity
+        activity = []
+        train_activity = []
+        test_activity = []
+        for j in range(act_data[i].shape[0]):   # traverse each file
+            cur_file = act_data[i][j]           # current file's data
+            length = cur_file.shape[0]          # number of samples in current file
+            segment_num = math.floor(length/32)         # number of segment
+            for k in range(segment_num):
+                activity.append(cur_file[k*segment_size:(k+1)*segment_size].T.flatten()[:97])
+            activity = np.array(activity)
+            print(activity)
+            return
+
+
+
+
 
 
 
