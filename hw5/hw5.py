189fc560c2cd73524ba990b063f1d1f836f18c30
laylalaisy <lsylayla@163.com>
2018-10-10 16:52:52 -0700

    update hw5

--- hw5/hw5.py
+++ hw5/hw5.py
@@ -2,7 +2,7 @@ import os
 import math
 import numpy as np
 import scipy as sp
-from sklearn import cluster
+from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
 act_num = 14
@@ -52,27 +52,34 @@ def split(act_data, percent, segment_size):
             cur_file = act_data[i][j]                   # current file's data
             length = cur_file.shape[0]                  # number of samples in current file
             segment_num = math.floor(length/32)         # number of segment
-            for k in range(segment_num):
+
+            for k in range(segment_num):                # build up segment
                 activity.append(cur_file[k*segment_size:(k+1)*segment_size].T.flatten()[:97])
             activity = np.array(activity)
 
-            train_num = math.floor(segment_num*percent)
+            train_num = math.floor(segment_num*percent) # split index of train and test set
             test_index = train_num
 
-            for m in range(train_num):
+            for m in range(train_num):                  # train
                 train_activities.append(activity[m])
 
-            for n in range(test_index, segment_num):
+            for n in range(test_index, segment_num):    # test
                 test_activities.append(activity[n])
 
     train_activities = np.array(train_activities)
     test_activities = np.array(test_activities)
-    return(train_activities.shape, test_activities.shape)
+
+    return train_activities, test_activities
 
 
 
-def execute(act_data, segment_size=32, cluster_size=40*12, percent=0.9, matrix_output=True):
+def execute(act_data, segment_size, cluster_size, percent, matrix_output):
     act_train, act_test = split(act_data, percent, segment_size)
+
+    kmeans = KMeans(n_clusters=cluster_size, random_state=0).fit(act_train[:, :96])
+    values = kmeans.cluster_centers_
+    labels = kmeans.labels_
+
 
 
 if __name__ == "__main__":
