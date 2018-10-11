698552fe0356a01f435935f69afc463aad104de3
laylalaisy <lsylayla@163.com>
2018-10-10 18:20:05 -0700

    update hw5

--- hw5/hw5.py
+++ hw5/hw5.py
@@ -46,25 +46,35 @@ def split(act_data, percent, segment_size):
     test_activities = []
 
     for i in range(act_num): # each activity
-        for j in range(act_data[i].shape[0]):           # traverse each file
-            activity = []                               # data of activity after flatten
+        train_activity = []
+        test_activity = []
 
+        file_num = act_data[i].shape[0]
+        test_num = math.floor(file_num*(1-percent))                 # number of test files/signals
+        if(test_num < 1):
+            test_num = 1
+        train_num = file_num-test_num                   # number of train files/signals
+
+        for j in range(train_num):                      # traverse each train file
             cur_file = act_data[i][j]                   # current file's data
             length = cur_file.shape[0]                  # number of samples in current file
             segment_num = math.floor(length/32)         # number of segment
 
             for k in range(segment_num):                # build up segment
-                activity.append(cur_file[k*segment_size:(k+1)*segment_size].T.flatten()[:97])
-            activity = np.array(activity)
+                train_activity.append(cur_file[k*segment_size:(k+1)*segment_size].T.flatten()[:97])
 
-            train_num = math.floor(segment_num*percent) # split index of train and test set
-            test_index = train_num
+        for j in range(train_num, train_num+test_num):                      # traverse each train file
+            cur_file = act_data[i][j]                   # current file's data
+            length = cur_file.shape[0]                  # number of samples in current file
+            segment_num = math.floor(length/32)         # number of segment
 
-            for m in range(train_num):                  # train
-                train_activities.append(activity[m])
+            for k in range(segment_num):                # build up segment
+                test_activity.append(cur_file[k*segment_size:(k+1)*segment_size].T.flatten()[:97])
 
-            for n in range(test_index, segment_num):    # test
-                test_activities.append(activity[n])
+        train_activity = np.array(train_activity)
+        train_activities.append(train_activity)
+        test_activity = np.array(test_activity)
+        test_activities.append(test_activity)
 
     train_activities = np.array(train_activities)
     test_activities = np.array(test_activities)
@@ -72,13 +82,14 @@ def split(act_data, percent, segment_size):
     return train_activities, test_activities
 
 
-
 def execute(act_data, segment_size, cluster_size, percent, matrix_output):
     act_train, act_test = split(act_data, percent, segment_size)
 
-    kmeans = KMeans(n_clusters=cluster_size, random_state=0).fit(act_train[:, :96])
-    values = kmeans.cluster_centers_
-    labels = kmeans.labels_
+    print(act_train[0])
+
+    # kmeans = KMeans(n_clusters=cluster_size, random_state=0).fit(act_train[:, :96])
+    # values = kmeans.cluster_centers_
+    # labels = kmeans.labels_
 
 
 
