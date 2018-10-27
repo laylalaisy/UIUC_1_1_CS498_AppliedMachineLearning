003bda071fb4b220597ffa191d3939f54c3a0c4d
laylalaisy <lsylayla@163.com>
2018-10-27 09:45:25 -0700

    update 12.3

--- hw7/4/4.R
+++ hw7/4/4.R
@@ -2,8 +2,6 @@ library(glmnet)
 library(klaR)
 library(caret)
 
-data(QuickStartExample);
-
 # read in training data and label
 data <- t(as.matrix(read.table("/home/layla/Desktop/cs498/hw7/4/train_data.txt", header=FALSE)));
 label <- as.matrix(read.table("/home/layla/Desktop/cs498/hw7/4/train_label.txt", header=FALSE));
