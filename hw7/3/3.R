1956f1b78e66ce3164d33dc519ebbd9e96a53006
laylalaisy <lsylayla@163.com>
2018-10-28 22:23:43 -0700

    update 12.3 & 12.4

--- hw7/3/3.R
+++ hw7/3/3.R
@@ -1,15 +1,80 @@
 library(glmnet)
+library(klaR)
+library(caret)
 
+curpath <- getwd();
+
+# TRAIN
 # read in training data and label
-train <- as.matrix(read.csv(file="/home/layla/Desktop/cs498/hw7/3/data/train/blogData_train.csv", header=FALSE, sep=","));
+filename <- paste(curpath, "/blogData_train.csv", sep="");
+train <- as.matrix(read.csv(file=filename, header=FALSE, sep=","));
 train_x <- train[,1:280];
 train_y <- train[,281];
 
 # fit
 cvfit = cv.glmnet(train_x, train_y, family = "poisson", alpha=1);
-plot(cvfit);
+png(file="1.png");
+plot(cvfit);    # plot of the cross-validated deviance of the model against the regularization variable
+dev.off();
 
 # predict
 print(cvfit$lambda.min);
 train_predict_y <- predict(cvfit, newx=train_x, type="response", s = "lambda.min");
-plot(train_y, train_predict_y, main="Train Data", xlab="true values", ylab="predicted values")
+# draw plot
+png(file="2_train_min.png");
+plot(train_y, train_predict_y, main="2_min Train Data", xlab="true values", ylab="predicted values");
+dev.off();
+
+# predict
+print(cvfit$lambda.1se);
+train_predict_y <- predict(cvfit, newx=train_x, type="response", s = "lambda.1se");
+# draw plot
+png(file="2_train_1se.png");
+plot(train_y, train_predict_y, main="2_1se Train Data", xlab="true values", ylab="predicted values");
+dev.off();
+
+
+# TEST
+# read in testing data and label
+testpath <- paste(curpath, "/data/test/", sep="");
+files <- list.files(path=testpath);
+num <- length(files);
+
+for (i in 1:num){
+  filename <- paste(testpath, files[i], sep="");
+  test <- as.matrix(read.csv(file=filename, header=FALSE, sep=","));
+  test_x <- test[,1:280];
+  test_y <- test[,281];
+  
+  # predict
+  test_predict_y <- predict(cvfit, newx=test_x, type="response", s = "lambda.min");
+  # draw plot
+  plotname <- paste("3_test_min", i, sep="");
+  plotname <- paste(plotname, ".png", sep="");
+  png(file=plotname);
+  plot(test_y, test_predict_y, main=plotname, xlab="true values", ylab="predicted values");
+  dev.off()
+  
+  # predict
+  test_predict_y <- predict(cvfit, newx=test_x, type="response", s = "lambda.1se");
+  # draw plot
+  plotname <- paste("3_test_1se", i, sep="");
+  plotname <- paste(plotname, ".png", sep="");
+  png(file=plotname);
+  plot(test_y, test_predict_y, main=plotname, xlab="true values", ylab="predicted values");
+  dev.off()
+}
+
+filename <- paste(curpath, "/blogData_test.csv", sep="");
+test <- as.matrix(read.csv(file=filename, header=FALSE, sep=","));
+test_x <- test[,1:280];
+test_y <- test[,281];
+  
+# predict
+test_predict_y <- predict(cvfit, newx=test_x, type="response", s = "lambda.min");
+
+# draw plot
+png(file="3_test.png");
+plot(test_y, test_predict_y, main="Test Data", xlab="true values", ylab="predicted values");
+dev.off();
+
--- hw7/4/4.R
+++ hw7/4/4.R
@@ -2,9 +2,13 @@ library(glmnet)
 library(klaR)
 library(caret)
 
+curpath <- getwd();
+
 # read in training data and label
-data <- t(as.matrix(read.table("/home/layla/Desktop/cs498/hw7/4/train_data.txt", header=FALSE)));
-label <- as.matrix(read.table("/home/layla/Desktop/cs498/hw7/4/train_label.txt", header=FALSE));
+datafile <- paste(curpath, "/train_data.txt", sep="");
+labelfile <- paste(curpath, "/train_label.txt", sep="");
+data <- t(as.matrix(read.table(datafile, header=FALSE)));
+label <- as.matrix(read.table(labelfile, header=FALSE));
 
 # extract train data by randomly choosing 80% from origin dataset
 train_index <- createDataPartition(y=label, p=0.8, list=FALSE);
@@ -18,12 +22,22 @@ test_y <- label[-train_index, 1];
 
 # fit: cross-validation
 cvfit = cv.glmnet(train_x, train_y, family = "binomial", type.measure = "class");
+png(file="cvfit.png");
 plot(cvfit);
+dev.off();
 
 # predict
 print(cvfit$lambda.min);
 test_predict_y <- predict(cvfit, newx=test_x , type="class", s = "lambda.min");
+# accuracy
+accuracy_table <- table(test_predict_y == test_y);
+print(accuracy_table);
+accuracy <- accuracy_table[2]/(accuracy_table[1]+accuracy_table[2])
+print(accuracy);
 
+# predict
+print(cvfit$lambda.1se);
+test_predict_y <- predict(cvfit, newx=test_x , type="class", s = "lambda.1se");
 # accuracy
 accuracy_table <- table(test_predict_y == test_y);
 print(accuracy_table);
