abe3f7087869b804177d7c027adcfeda9ef3e036
laylalaisy <lsylayla@163.com>
2018-12-05 09:48:12 -0800

    update cnn

--- hw10/cnn_mnist.py
+++ hw10/cnn_mnist.py
@@ -41,7 +41,67 @@ def cnn_model_fc(features, label, mode):
 		)
 
 	# Pooling Layer #2
-	
+	pool2 = tf.layers.max_pooling2d(
+		inputs=conv2,
+		pool_size=[2, 2],
+		strides=2
+		)
+
+	# Dense Layer
+	pool2_flat = tf.reshape(pool2, [-1, 7*7*64])
+	dense = tf.layers.dense(
+		inputs=pool2_falt,
+		units=1024,
+		activation=tf.nn.relu
+		)
+	dropout = tf.layers.dropout(
+		inputs=dense,
+		rate=0.4,
+		training=mode == tf.estimator.ModeKeys.TRAIN
+		)
+
+	# Logits Layer
+	logits = tf.layers.dense(
+		inputs=dropout,
+		units=10
+		)
+
+	# Prediction
+	predictions={
+		# Generate predictions (for PREDICT and EVAL mode)
+		"classes": tf.argmax(input=logits, axis=1),
+		# Add 'softmax_tensor' to the graph. It is used for PREDICT and by the 'logging_hook'
+		"probabilities": tf.nn.softmax(logits, name="softmax_tensor")
+	}
+
+	if mode == tf.estimator.ModeKeys.PREDICT:
+		return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+
+	# Calculate Loss (for both TRAIN and EVAL modes)
+	loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+
+	# Configure the Training Optimizer (for TRAIN mode)
+	if mode == tf.estimator.ModeKeys.TRAIN:
+		optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+		train_op = optimizer.minimize(
+			loss=loss,
+			global_step=tf.train.get_global_step()
+			)
+		return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+
+	# Add evaluation metrics (for EVAL mode)
+	eval_metric_ops = {
+		"accuracy": tf.metrics.accuracy(
+			labels=labesl,
+			predictions=predictions["classes"]
+		)
+	}
+	return tf.estimator.EstimatorSpec(
+		mode=mode,
+		loss=loss,
+		eval_metric_ops=eval_metric_ops
+	)
+
 
 if __name__ == "__main__":
 	tf.app.run()
